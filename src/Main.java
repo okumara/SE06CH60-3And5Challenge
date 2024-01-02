a6dc5ff18160ba501ee2193f5c3a72e0be9401bb
okumara <oeta.kumara@gmail.com>
2024-01-02 22:45:00 +0100

    First version for review

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int counter=0;
+        int sum=0;
+
+        for (int i=1; i<=1000 && counter <= 5; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                counter++;
+                sum += i;
+                System.out.println (counter + ": i = " + i + " / sum = " + sum);
+            }
+        }
+        System.out.println("sum = " + sum);
     }
 }
